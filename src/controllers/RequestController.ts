9c277eed3d0b2421bb420f5f26e8a11f70da777f
Lê Quý Hoàng Tâm <127521514+ellie2222222@users.noreply.github.com>
2025-03-16 17:12:17 +0700

    chore: fix role middleware, update route, fix update user, adjust validator, adjust swagger

--- src/controllers/RequestController.ts
+++ src/controllers/RequestController.ts
@@ -47,11 +47,12 @@ class RequestController {
 
   getAllRequests = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const { page, size, search, order, sortBy, status } = req.query;
+      const { size, search, order, sortBy, status } = req.query;
+      const queryPage = req.query.page;
 
-      const requests = await this.requestService.getAllRequests(
+      const { requests, page, total, totalPages } = await this.requestService.getAllRequests(
         {
-          page: parseInt(page as string) || 1,
+          page: parseInt(queryPage as string) || 1,
           size: parseInt(size as string) || 10,
           search: search as string,
           order: (order as "ascending" | "descending") || "ascending",
@@ -62,7 +63,7 @@ class RequestController {
 
       res
         .status(StatusCodeEnum.OK_200)
-        .json({ requests, message: "Get all request successfully" });
+        .json({ requests, page, total, totalPages, message: "Get all request successfully" });
     } catch (error) {
       next(error);
     }
